7d320263f29d4d6c08cd466598431e327190b759
jaobombonato <jaobombonato@gmail.com>
2025-11-27 19:09:19 -0300

    Update preview.js

--- assets/js/preview.js
+++ assets/js/preview.js
@@ -1,4 +1,4 @@
-window.renderPreview = function (compiledCode) {
+window.renderPreview = function (userCode) {
 
     const iframe = document.getElementById("previewFrame");
 
@@ -7,21 +7,33 @@ window.renderPreview = function (compiledCode) {
         <body>
             <div id="root"></div>
 
-            <!-- Carrega Babel dentro do IFRAME -->
+            <!-- Babel dentro do iframe -->
             <script src="https://unpkg.com/@babel/standalone/babel.min.js"><\/script>
 
             <script type="module">
                 import React from "https://esm.sh/react@18";
                 import ReactDOM from "https://esm.sh/react-dom@18";
 
+                // Compila o TSX do usuário
+                const compiled = Babel.transform(${JSON.stringify(
+                    String.raw`${userCode}`
+                )}, {
+                    presets: [
+                        ["typescript", { allExtensions: true, isTSX: true }],
+                        ["react", { runtime: "automatic" }]
+                    ]
+                }).code;
+
+                // Executa o código compilado
                 const App = (function(){
-                    ${compiledCode}
+                    ${'${compiled}'}
                     return exports.default || module.exports || window.App;
                 })();
 
                 ReactDOM.createRoot(document.getElementById("root"))
                     .render(React.createElement(App));
             <\/script>
+
         </body>
     </html>
     `;
