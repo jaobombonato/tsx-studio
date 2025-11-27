cf7ea0c5c8571afde7c3b9551be041d41a94ed94
jaobombonato <jaobombonato@gmail.com>
2025-11-27 19:25:56 -0300

    Update preview.js

--- assets/js/preview.js
+++ assets/js/preview.js
@@ -3,41 +3,41 @@ window.renderPreview = function (tsxCode) {
 
     const html = `
     <html>
-        <body>
-            <div id="root"></div>
+    <body>
+        <div id="root"></div>
 
-            <!-- Babel dentro do IFRAME -->
-            <script src="https://unpkg.com/@babel/standalone/babel.min.js"><\/script>
+        <!-- Babel dentro do IFRAME -->
+        <script src="https://unpkg.com/@babel/standalone/babel.min.js"><\/script>
 
-            <script type="module">
-                import React from "https://esm.sh/react@18";
-                import ReactDOM from "https://esm.sh/react-dom@18";
+        <script type="module">
+            import React from "https://esm.sh/react@18";
+            import ReactDOM from "https://esm.sh/react-dom@18";
 
-                // Compila TSX dentro do iframe
-                const compiled = Babel.transform(\`${tsxCode}\`, {
-                    presets: [
-                        ["typescript", { allExtensions: true, isTSX: true }],
-                        ["react", { runtime: "automatic" }]
-                    ]
-                }).code;
+            // Compila TSX dentro do iframe
+            const compiled = Babel.transform(\`${tsxCode}\`, {
+                presets: [
+                    ["typescript", { allExtensions: true, isTSX: true }],
+                    ["react", { runtime: "classic" }] // ← SOLUÇÃO
+                ]
+            }).code;
 
-                // Executa o JS compilado
-                const App = (function(){
-                    try {
-                        let exports = {};
-                        eval(compiled);
-                        return exports.default || window.App;
-                    } catch (err) {
-                        document.body.innerHTML = '<pre style="color:red;">Erro no código: ' + err.message + '</pre>';
-                        throw err;
-                    }
-                })();
+            // Executa o JS compilado
+            const App = (function(){
+                try {
+                    let exports = {};
+                    eval(compiled);
+                    return exports.default || window.App;
+                } catch (err) {
+                    document.body.innerHTML = '<pre style="color:red;font-size:18px;">Erro no código: ' + err.message + '</pre>';
+                    throw err;
+                }
+            })();
 
-                ReactDOM.createRoot(document.getElementById("root"))
-                    .render(React.createElement(App));
-            <\/script>
+            ReactDOM.createRoot(document.getElementById("root"))
+                .render(React.createElement(App));
+        <\/script>
 
-        </body>
+    </body>
     </html>
     `;
 
