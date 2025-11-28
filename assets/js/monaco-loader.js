b4ef170fbb7e2ac139ea92194557247f5d800b4c
jaobombonato <jaobombonato@gmail.com>
2025-11-28 15:53:11 -0300

    Update monaco-loader.js

--- assets/js/monaco-loader.js
+++ assets/js/monaco-loader.js
@@ -2,17 +2,18 @@
 
 window.MonacoEnvironment = {
   getWorkerUrl: function (_, label) {
-    const path = "https://cdn.jsdelivr.net/npm/monaco-editor@0.45.0/min/vs";
-    if (label === "json") return `${path}/language/json/json.worker.js`;
-    if (label === "css") return `${path}/language/css/css.worker.js`;
-    if (label === "html") return `${path}/language/html/html.worker.js`;
+    const base = "https://cdn.jsdelivr.net/npm/monaco-editor@0.45.0/min/vs";
+    if (label === "json") return `${base}/language/json/json.worker.js`;
+    if (label === "css") return `${base}/language/css/css.worker.js`;
+    if (label === "html") return `${base}/language/html/html.worker.js`;
     if (label === "ts" || label === "typescript")
-      return `${path}/language/typescript/ts.worker.js`;
-    return `${path}/editor/editor.worker.js`;
+      return `${base}/language/typescript/ts.worker.js`;
+    return `${base}/editor/editor.worker.js`;
   }
 };
 
 require.config({
+  waitSeconds: 15, // evita timeout em 3G/WebView/Safari
   paths: {
     vs: "https://cdn.jsdelivr.net/npm/monaco-editor@0.45.0/min/vs"
   }
